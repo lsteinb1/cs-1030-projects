434d984328fef1a69617598025c1e3746b895e59
lsteinb1 <lsteinb1@slcc.edu>
2025-11-06 13:41:29 -0700

    combined both versions of program 1

--- Steinberg_Lydia_loops.py
+++ Steinberg_Lydia_loops.py
@@ -16,18 +16,47 @@ def add():
 def evens():
     print("Printing every even number from 0 to 30.")
     i = 0
-    even_nums = []
+    even_num = 0
     for i in range(0, 31, 2):
-        even_nums.append(i)
-    print(even_nums)
+        even_num = i
+        print(even_num, end = " ")
 
-def multiplication_table():
-    pass
+def mult_table():
+    k = int(input("Input an integer: "))
+    i = 0
+    for i in range(1, 11):
+        print(f"{k} × {i} = ", k * i)
+
+def n_numbers():
+    n = 1
+    try:
+        n = int(input("How many numbers would you like to enter?: "))
+    except:
+        pass
+    i = 0
+    numbers = []
+    num_sum = 0
+    num_avg = 0
+    for i in range(n):
+        num = int(input("Input an integer: "))
+        numbers.append(num)
+        num_sum += num
+    i = 0
+    print("Entered numbers:", end = " ")
+    for i in range(n):
+        if i != n:
+            print(numbers[i], end = ", ")
+        else:
+            print(numbers[i], end = ".")
+    num_avg = (num_sum / n)
+    print(f"Sum: {num_sum}, Average: {num_avg}")
+
 
 def main():
     count()
     add()
     evens()
-    multiplication_table()
+    mult_table()
+    n_numbers()
 
 main()
